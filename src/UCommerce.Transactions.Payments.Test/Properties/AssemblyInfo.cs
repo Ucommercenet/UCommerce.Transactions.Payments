a186744e521ed6eccc62c36dad7b328178908cd1
Lukas <lva@ucommerce.dk>
2021-02-19 14:27:43 +0100

    chore: update AssemblyInfo.cs

--- src/UCommerce.Transactions.Payments.Test/Properties/AssemblyInfo.cs
+++ src/UCommerce.Transactions.Payments.Test/Properties/AssemblyInfo.cs
@@ -31,7 +31,7 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("9.1.0.20160")]
-[assembly: AssemblyVersion("9.1.0.20160")]
-[assembly: AssemblyFileVersion("9.1.0.20160")]
-[assembly: AssemblyInformationalVersion("9.1.0.20160 ")]
+// [assembly: AssemblyVersion("9.4.0.21050")]
+[assembly: AssemblyVersion("9.4.0.21050")]
+[assembly: AssemblyFileVersion("9.4.0.21050")]
+[assembly: AssemblyInformationalVersion("9.4.0.21050")]
