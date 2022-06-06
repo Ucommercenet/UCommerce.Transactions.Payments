d70453112e465b7bf3627fa33cd3de19c49c4ab7
Bjarne Fyrstenborg <bf@mcb.dk>
2022-06-06 18:22:51 +0200

    Initial commit

---
+++ src/UCommerce.Transactions.Payments.Nets/NetsEasyPaymentMethodService.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Text.RegularExpressions;
+using System.Web;
+using Ucommerce.EntitiesV2;
+using Ucommerce.Extensions;
+using Ucommerce.Infrastructure;
+using Ucommerce.Infrastructure.Environment;
+using Ucommerce.Infrastructure.Logging;
+using Ucommerce.Transactions.Payments.Common;
+using Ucommerce.Transactions.Payments.Nets;
+
+namespace Ucommerce.Transactions.Payments.Dibs
+{
+	/// <summary>
+	/// Implementation of the Nets Easy payment provider.
+	/// </summary>
+	public class NetsEasyPaymentMethodService : ExternalPaymentMethodService
+	{
+		private readonly IWebRuntimeInspector _webRuntimeInspector;
+	    private readonly ILoggingService _loggingService;
+		private AbstractPageBuilder PageBuilder { get; set; }
+
+		public NetsEasyPaymentMethodService(NetsPageBuilder pageBuilder, IWebRuntimeInspector webRuntimeInspector, ILoggingService loggingService)
+		{
+			_webRuntimeInspector = webRuntimeInspector;
+		    _loggingService = loggingService;
+			PageBuilder = pageBuilder;
+		}
+
+		/// <summary>
+		/// Renders the page with the information needed by the payment provider.
+		/// </summary>
+		/// <param name="paymentRequest">The payment request.</param>
+		/// <returns>The html rendered.</returns>
+		public override string RenderPage(PaymentRequest paymentRequest)
+		{
+			return PageBuilder.Build(paymentRequest);
+		}
+
+		/// <summary>
+		/// Processes the callback and excecutes a pipeline if there is one specified for this paymentmethodservice.
+		/// </summary>
+		/// <param name="payment">The payment to process.</param>
+		public override void ProcessCallback(Payment payment)
+		{
+			Guard.Against.MissingHttpContext(_webRuntimeInspector);
+			//Guard.Against.MissingRequestParameter("transact");
+			Guard.Against.PaymentNotPendingAuthorization(payment);
+
+			//string transactionId = GetTransactionParameter();
+
+			var hashVeryfied = false; //VerifyMd5Hash(payment);
+
+			//payment.TransactionId = transactionId;
+			payment.PaymentStatus = PaymentStatus.Get((int)PaymentStatusCode.Authorized);
+			ProcessPaymentRequest(new PaymentRequest(payment.PurchaseOrder, payment));
+		}
+
+		/// <summary>
+		/// Acquires the payment from the payment provider. This is often used when you need to call external services to handle the acquire process.
+		/// </summary>
+		/// <param name="payment">The payment.</param>
+		/// <param name="status">The status.</param>
+		/// <returns></returns>
+		protected override bool AcquirePaymentInternal(Payment payment, out string status)
+		{
+			string merchant = payment.PaymentMethod.DynamicProperty<string>().Merchant.ToString();
+			
+			string amount = payment.Amount.ToCents().ToString();
+			var referenceId = payment.ReferenceId;
+			var transactionId = payment.TransactionId;
+
+            status = null;
+
+            return false;
+		}
+
+		/// <summary>
+		/// Refunds the payment from the payment provider. This is often used when you need to call external services to handle the refund process.
+		/// </summary>
+		/// <param name="payment">The payment.</param>
+		/// <param name="status">The status.</param>
+		/// <returns></returns>
+		protected override bool RefundPaymentInternal(Payment payment, out string status)
+		{
+			string merchant = payment.PaymentMethod.DynamicProperty<string>().Merchant.ToString();
+
+			var amount = payment.Amount.ToCents().ToString();
+
+			var transactionId = payment.TransactionId;
+			var referenceId = payment.ReferenceId;
+
+			status = null;
+
+			return false;
+		}
+
+		/// <summary>
+		/// Cancels the payment from the payment provider. This is often used when you need to call external services to handle the cancel process.
+		/// </summary>
+		/// <param name="payment">The payment.</param>
+		/// <param name="status">The status.</param>
+		/// <returns></returns>
+		protected override bool CancelPaymentInternal(Payment payment, out string status)
+		{
+			string merchant = payment.PaymentMethod.DynamicProperty<string>().Merchant.ToString();
+
+			var referenceId = payment.ReferenceId;
+
+			status = null;
+
+			return false;
+		}
+	}
+}
---
+++ src/UCommerce.Transactions.Payments.Nets/NetsPageBuilder.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections.Generic;
+using System.Text;
+using Ucommerce.Content;
+using Ucommerce.Extensions;
+using Ucommerce.Transactions.Payments.Common;
+using Ucommerce.Web;
+
+namespace Ucommerce.Transactions.Payments.Nets
+{
+    public class NetsPageBuilder : AbstractPageBuilder
+    {
+	    private readonly IAbsoluteUrlService _absoluteUrlService;
+	    private readonly ICallbackUrl _callbackUrl;
+	    private IDomainService DomainService { get; set; }
+
+        public NetsPageBuilder(IDomainService domainService, IAbsoluteUrlService absoluteUrlService, ICallbackUrl callbackUrl)
+        {
+	        _absoluteUrlService = absoluteUrlService;
+	        _callbackUrl = callbackUrl;
+	        DomainService = domainService;
+        }
+
+    	protected override void BuildHead(StringBuilder page, PaymentRequest paymentRequest)
+        {
+            page.Append("<title>Nets</title>");
+            page.Append(@"<script type=""text/javascript"" src=""//ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js""></script>");
+            if(!Debug)
+                page.Append(@"<script type=""text/javascript"">$(function(){ $('form').submit();});</script>");
+        }
+
+		protected virtual IDictionary<string, string> GetParameters(PaymentRequest paymentRequest)
+		{
+			string merchant = paymentRequest.PaymentMethod.DynamicProperty<string>().Merchant;
+
+			string language = paymentRequest.PaymentMethod.DynamicProperty<string>().Language;
+			string termsUrl = paymentRequest.PaymentMethod.DynamicProperty<string>().TermsUrl;
+			string merchantTermsUrl = paymentRequest.PaymentMethod.DynamicProperty<string>().MerchantTermsUrl;
+
+			string testSecretKey = paymentRequest.PaymentMethod.DynamicProperty<string>().TestSecretKey;
+			string liveSecretKey = paymentRequest.PaymentMethod.DynamicProperty<string>().LiveSecretKey;
+
+			string acceptUrl = paymentRequest.PaymentMethod.DynamicProperty<string>().AcceptUrl;
+			string cancelUrl = paymentRequest.PaymentMethod.DynamicProperty<string>().CancelUrl;
+			string callBackUrl = paymentRequest.PaymentMethod.DynamicProperty<string>().CallbackUrl;
+
+			string paymentMethods = paymentRequest.PaymentMethod.DynamicProperty<string>().PaymentMethods;
+			bool autoCapture = paymentRequest.PaymentMethod.DynamicProperty<bool>().AutoCapture;
+			bool testMode = paymentRequest.PaymentMethod.DynamicProperty<bool>().TestMode;
+
+			var currency = paymentRequest.Amount.CurrencyIsoCode;
+			string amount = paymentRequest.Payment.Amount.ToCents().ToString();
+
+			var parametersToReturn = new Dictionary<string, string>();
+
+			parametersToReturn["merchant"] = merchant;
+			parametersToReturn["accepturl"] = _absoluteUrlService.GetAbsoluteUrl(acceptUrl);
+			
+			if (!string.IsNullOrWhiteSpace(cancelUrl))
+				parametersToReturn["cancelurl"] = _absoluteUrlService.GetAbsoluteUrl(cancelUrl);
+
+			parametersToReturn["orderGuid"] = paymentRequest.Payment.PurchaseOrder.OrderGuid.ToString();
+			parametersToReturn["currency"] = currency;
+			//parametersToReturn["language"] = GetTwoLetterLanguageName();
+
+			parametersToReturn["callbackurl"] = _callbackUrl.GetCallbackUrl(callBackUrl, paymentRequest.Payment);
+
+			if (testMode)
+				parametersToReturn["test"] = "1";
+
+			parametersToReturn["orderid"] = paymentRequest.Payment.ReferenceId;
+			parametersToReturn["amount"] = amount;
+
+			return parametersToReturn;
+		}
+
+	    protected virtual string GetTwoLetterLanguageName()
+	    {
+			Domain domain = DomainService.GetCurrentDomain();
+		    return domain != null
+			    ? domain.Culture.TwoLetterISOLanguageName
+			    : System.Threading.Thread.CurrentThread.CurrentCulture.TwoLetterISOLanguageName;
+	    }
+
+		protected virtual void AddParameters(StringBuilder page, IDictionary<string,string> parametersToAdd)
+		{	
+			foreach (var parameter in parametersToAdd)
+			{
+				AddHiddenField(page,parameter.Key,parameter.Value);
+			}
+
+			if (Debug)
+				AddSubmitButton(page, "ac", "Post it");
+	    }
+
+        protected override void BuildBody(StringBuilder page, PaymentRequest paymentRequest)
+        {
+            page.Append(@"<form method=""post"" action=""https://payment.architrade.com/paymentweb/start.action"">");
+
+	        var parameters = GetParameters(paymentRequest);
+			AddParameters(page,parameters);
+
+            page.Append("</form>");
+        }
+    }
+}
